7df5ead1d412825a67f5105310b59d2c071feac6
tuhincoder <mdtuhinmahmudul@gmail.com>
2026-01-05 15:50:57 +0600

    server domoin added

--- src/api/useAxiosPublic.jsx
+++ src/api/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://restaurant-server-delta-lyart.vercel.app",
   // timeout: 1000,
 });
 
